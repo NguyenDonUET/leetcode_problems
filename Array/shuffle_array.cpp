0b74947a4d9be964d553cdca5b144fbdd5365814
NguyenDonUET <nguyendoncb@gmail.com>
2021-12-02 20:58:10 +0700

    easy

--- Array/shuffle_array.cpp
+++ Array/shuffle_array.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+Cho mảng nums có 2n phần tử có dạng [x1,x2,...,xn,y1,y2,...,yn].
+Trả về 1 mảng mới có dạng [x1,y1,x2,y2,...,xn,yn]
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7] 
+*/
 vector<int> shuffle(vector<int> &nums, int n)
 {
     vector<int> result(n * 2);
